f9676f59d418966a8e420be547eaab2accb5f3f3
Manisha Shek <manishas@infinite.com>
2023-09-08 10:28:25 +0530

    This is my !st git

---
+++ ASSESMENT-2/ConsoleApp1/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using  static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+
+        {
+
+            static int[] a = new int[5];
+
+            public static void input()
+
+            {
+
+                WriteLine("enter the input marks:.........\n ");
+
+                for (int i = 0; i < a.Length; i++)
+
+                {
+
+                    a[i] = Convert.ToInt32(ReadLine());
+
+                }
+
+                WriteLine("array sorted in asending order:.........\n");
+
+                System.Array.Sort(a);
+
+                for (int j = 0; j < a.Length; j++)
+
+                    WriteLine($"    {a[j]}  ");
+
+                WriteLine("array in desending order..............\n");
+
+                System.Array.Reverse(a);
+
+                for (int k = 0; k < a.Length; k++)
+
+                    WriteLine($" {a[k]} ");
+
+
+            }
+
+            public static void total()
+
+            {
+
+                int tt = 0;
+
+                for (int k = 0; k < a.Length; k++)
+
+                    tt = tt + a[k];
+
+                WriteLine($" total value {tt}");
+
+                WriteLine($" the maximum value={a.Max()}");
+
+                WriteLine($" the minimum value={a.Min()}");
+
+                WriteLine($" the Average of marks={a.Average()}");
+
+            }
+
+            public static void Main(string[] args)
+
+            {
+
+
+                input();
+
+                total();
+
+
+                //
+
+                // WriteLine($"Maximum of array {arry.Max}");
+
+                //WriteLine($"Minimum of array {arry.Min}");
+
+
+
+
+
+                WriteLine(" enter the row size of 2d array ");
+
+                int r = Convert.ToInt32(ReadLine());
+
+                Write(" enter the colomn size\n");
+
+                int c = Convert.ToInt32(ReadLine());
+
+                int[,] arr = new int[r, c];
+
+                WriteLine("enter the input values for  ");
+
+                for (int i = 0; i < r; i++)
+
+                {                                   //we can use arr.getlength(0) to get length of row;
+
+                    for (int j = 0; j < c; j++)
+
+                    {                               // we can use arr.getlenth(10 to get length of column;
+
+                        arr[i, j] = Convert.ToInt32(ReadLine());
+
+                    }
+
+                }
+
+                for (int i = 0; i < r; i++)
+
+                {
+
+                    for (int j = 0; j < c; j++)
+
+                    {
+
+                        Write($" { arr[i, j] } ");
+
+                        Write("\n");
+
+                    }
+
+                }
+
+                Read();
+
+
+
+
+
+            }
+
+        }
+
+    }
+
---
+++ Assesment-1/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System
+namespace ConsoleApp1
+{
+    class Program
+    {
+         int a, b;
+        public static void equal()
+        {
+            WriteLine(" enter the number1");
+            a = Convert.ToInt32(Console.ReadLine());
+            WriteLine("enter the number2");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a == b)
+            {
+                WriteLine(" equal");
+            }
+            else
+            {
+                WriteLine(" not equal");
+            }
+
+
+
+        }
+        public static void negative(params int[] args)
+        {
+            fo (int i in args)
+            {
+                if (i < 0)
+                {
+                    WriteLine($" {i} is negative ");
+                }
+                else
+                {
+                    WriteLine($" {i} is positive ");
+                }
+            }
+        }
+        public static void operation()
+        {
+            WriteLine(" enter the values for operation");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            int add = a + b;
+            int sub = a - b;
+            int multiplication = a * b;
+            float division = a / b;
+            WriteLine($"sum {add} difference {sub} product {multiplication} division {division}");
+        }
+        static void Main(string[] args)
+        {
+            equal();
+            operation();
+            negative(14);
+            Read();
+
+
+
+
+
+        }
+    }
+}
+
