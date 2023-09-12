6c7e88a8777ed4beac58e579128bce909fcbf5d2
Manisha Shek <manishas@infinite.com>
2023-09-12 21:20:59 +0530

    my assignment

---
+++ ASSIGNMENT-4/Accounts.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSESMENT_4
+{
+    class accounts
+    {
+        public int accountnum;
+        public string CustomerName;
+        public string accounttype;
+
+
+
+        public accounts(int accountnum, string customerName, string accounttype)
+        {
+            this.accountnum = accountnum;
+            this.CustomerName = customerName;
+            this.accounttype = accounttype;
+
+        }
+        public void Displaydetails()
+        {
+            Console.WriteLine($"Account Number :{accountnum}");
+            Console.WriteLine($"Your Name :{CustomerName}");
+            Console.WriteLine($"Your Account Type:{accounttype}");
+
+        }
+        public static void credit()
+        {
+            Console.WriteLine("Enter the deposited amount:");
+            int DepositAmount = Convert.ToInt32(Console.ReadLine());
+            int balance = 20000;
+            if (DepositAmount <= 0)
+            {
+                Console.WriteLine("Entered amount is invalid");
+            }
+            else
+            {
+                balance = balance + DepositAmount;
+
+                Console.WriteLine("Total Balance is " + balance);
+            }
+        }
+        public static void debit()
+        {
+            Console.WriteLine("Enter the amount to be debited: ");
+            int debitAmount = Convert.ToInt32(Console.ReadLine());
+            int balance = 10000;
+            if (debitAmount == 0 || debitAmount > 10000)
+            {
+                Console.WriteLine("Entered amount is invalid");
+            }
+            else
+            {
+                balance = balance - debitAmount;
+
+                Console.WriteLine("Total Balance is " + balance);
+            }
+        }
+
+        class Program
+        {
+
+            public static void Main()
+            {
+
+                accounts info = new accounts(79845637, "Manisha", "savings account");
+                info.Displaydetails();
+                Console.WriteLine("Choose Transaction Type: ");
+                Console.WriteLine("1. Deposit");
+                Console.WriteLine("2. Withdrawal");
+
+                Console.WriteLine("Enter your transaction type 1 or 2: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    accounts.credit();
+                }
+                else if (choice == 2)
+                {
+                    accounts.debit();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Option");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ ASSIGNMENT-4/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSESMENT_4
+{
+    class student
+    {
+        public string rollnum;
+        public string name;
+        public string Class;
+        public char semester;
+        public string branch;
+        public int[] marks = new int[5];
+
+
+        public student(string rollnum, string name, string Class, char semester, string branch)
+        {
+            this.rollnum = rollnum;
+            this.name = name;
+            this.Class = Class;
+            this.semester = semester;
+            this.branch = branch;
+
+        }
+        public void displaydetails()
+        {
+            Console.WriteLine($"rollnum={rollnum},name={name},Class={Class},semester={semester},branch={branch}");
+
+        }
+
+        public void GetMarks()
+        {
+            Console.WriteLine("Enter the marks of 5 subjects");
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.Write($"Subject {i + 1}: ");
+                marks[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public void displayresult()
+        {
+            double totalMarks = 0;
+            foreach (int mark in marks)
+            {
+                totalMarks += mark;
+            }
+            double averageMarks = totalMarks / marks.Length;
+
+
+            if (marks.Any(mark => mark < 35))
+            {
+                Console.WriteLine("Result: Failed ");
+            }
+            else if (averageMarks < 50)
+            {
+                Console.WriteLine("Result: Failed )");
+            }
+            else
+            {
+                Console.WriteLine("Result: Passed");
+            }
+        }
+
+
+
+
+        public static void Main()
+        {
+            student details = new student("786", "manisha", "Btech", '8', "cse");
+            details.displaydetails();
+            details.GetMarks();
+            details.displayresult();
+            Console.ReadLine();
+        }
+
+    }
+
+}
+
