f82fa8a873bf95cea4e4294cdbc92b5acb05b287
Manisha Shek <manishas@infinite.com>
2023-09-23 18:58:05 +0530

    my git

---
+++ ASSIGNMENTS/ASSIGNMENT-6/Concession-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Concession;
+
+namespace Concession_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter your Name: ");
+            string Name = Console.ReadLine();
+
+            Console.Write("Enter your Age: ");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            Concession.ConcessionCalculator concessionCalculator = new Concession.ConcessionCalculator(Name, Age);
+            string result = concessionCalculator.CalculateConcession();
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ASSIGNMENTS/ASSIGNMENT-6/Concession/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Concession
+{
+    public class ConcessionCalculator
+    {
+        public const int TotalFare = 500;
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public ConcessionCalculator(string Name, int Age)
+        {
+           this. Name = Name;
+           this. Age = Age;
+        }
+
+        public string CalculateConcession()
+        {
+            if (Age <= 5)
+            {
+                return $"Little Champs - Free Ticket , Name: {Name}, Age: {Age}";
+            }
+            else if (Age >= 60)
+            {
+                double discountedFare = TotalFare * 0.3;
+                return $"Senior Citizen Calculated Fare: {discountedFare} , Name: {Name}, Age: {Age}";
+            }
+            else
+            {
+                return $"Ticket Booked - Fare: {TotalFare} , Name: {Name}, Age: {Age}";
+            }
+        }
+    }
+}
