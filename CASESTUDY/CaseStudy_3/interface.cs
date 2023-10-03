75ddff9f93fc75149394c0b0ed0b6e4348c6c551
Manisha Shek <manishas@infinite.com>
2023-10-03 22:11:16 +0530

    my git

---
+++ CASESTUDY/CaseStudy_3/interface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case_Study_3
+{
+    public interface IUserInterface
+    {
+        void ShowFirstScreen();
+        void ShowStudentScreen();
+        void ShowAdminScreen();
+        void ShowAllStudentsScreen();
+        void ShowStudentRegistrationScreen();
+        void IntroduceNewCourseScreen();
+        void ShowAllCoursesScreen();
+    }
+}
