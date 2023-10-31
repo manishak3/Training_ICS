4e238dfe0c92e7d80b46060e07faf8c1c9bc626b
Manisha Shek <manishas@infinite.com>
2023-10-31 17:34:56 +0530

    ASP

---
+++ ASP-ASSIGNMENT/ASP-Assignment/ASP.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ASP.aspx.cs" Inherits="ASP_ASSIGNMENT.ASP" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+   <form id="form1" runat="server">
+        <div>
+            
+            <asp:DropDownList ID="ddlItems" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlItems_SelectedIndexChanged">
+                <asp:ListItem Text="Select Item"/>
+                <asp:ListItem Text="DELL" Value="1"  />
+                <asp:ListItem Text="HP" Value="2"  />
+                <asp:ListItem Text="MAC" Value="3"  />
+                
+
+            </asp:DropDownList>
+            <br /><br />
+
+            <asp:Image ID="imgItem"  runat="server" ImageUrl="Images/default.jpg" Width="300" Height="300" />
+
+            <br /><br />
+
+            <asp:Button ID="btnShowCost" runat="server" Text="Show Cost" OnClick="btnShowCost_Click" />
+            <br /><br />
+
+            <asp:Label ID="lblCost" runat="server" Text="" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP-ASSIGNMENT/ASP-Assignment/ASP.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP_ASSIGNMENT
+{
+    public partial class ASP : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Set the default image when the page loads
+                imgItem.ImageUrl = "Images/default.jpg";
+            }
+        }
+
+        protected void btnShowCost_Click(object sender, EventArgs e)
+        {
+            
+            string SelectedItem = ddlItems.SelectedValue;
+            string cost = string.Empty;
+            if (SelectedItem == "1")
+            {
+
+                cost = "65000";
+
+            }
+
+            else if (SelectedItem == "2")
+            {
+
+                cost = "80000";
+            }
+
+            else if (SelectedItem == "3")
+            {
+
+                cost = "150000";
+            }
+           
+            string Item = ddlItems.SelectedItem.Text;
+
+            lblCost.Text = $"Cost of {Item}: {cost}";
+
+        }
+
+        protected void ddlItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // When an item is selected, update the image accordingly
+            string selectedItem = ddlItems.SelectedValue;
+            string cost = string.Empty;
+            if (selectedItem == "1")
+            {
+                imgItem.ImageUrl = $"Images/DELL.jpg";
+            }
+
+            else if (selectedItem == "2")
+            {
+                imgItem.ImageUrl = $"Images/HP.jpg";
+
+            }
+
+            else if (selectedItem == "3")
+            {
+                imgItem.ImageUrl = $"Images/MAC.jpg";
+
+            }
+             else
+            {
+
+                imgItem.ImageUrl = $"Images/{"Images/default.jpg"}";
+            }
+        }
+    }
+
+
+}
