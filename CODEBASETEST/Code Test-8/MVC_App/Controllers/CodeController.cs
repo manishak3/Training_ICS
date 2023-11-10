b061434a6157e14e896d83e7b40ea679fe85a17f
Manisha Shek <manishas@infinite.com>
2023-11-10 18:11:51 +0530

    my git

---
+++ CODEBASETEST/Code Test-8/MVC_App/Controllers/CodeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_App.Controllers
+{
+    public class CodeController : Controller
+    {
+        // GET: Code
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
