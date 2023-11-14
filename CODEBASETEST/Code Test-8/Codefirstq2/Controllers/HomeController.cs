7984bdba2393a8daa89e79cbc49cb46a8f7111ca
Manisha Shek <manishas@infinite.com>
2023-11-14 12:06:01 +0530

    my git

---
+++ CODEBASETEST/Code Test-8/Codefirstq2/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Codefirstq2.Models;
+using System.Data.Entity;
+namespace Codefirstq2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        MovieContext db = new MovieContext();
+        public ActionResult Index()
+        {
+            var movies = db.movies.ToList();
+            return View(movies);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(movie movie)
+        {
+            db.movies.Add(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var movie = db.movies.Find(id);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(movie movie)
+        {
+            db.movies.Add(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var movie = db.movies.Find(id);
+            db.movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ CODEBASETEST/Code Test-8/Codefirstq2/Models/movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Codefirstq2.Models
+{
+    public class movie
+    {
+        [Key]
+        public int mid { get; set; }
+        public string moviename { get; set; }
+        public string moviedate { get; set; }
+    }
+}
